3469e3678de6175bb3be4d8e44ef53d46ca34679
Pablo Pinto <pablo@g2i.co>
2025-04-03 10:36:24 -0500

    updated blog page tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,20 +4,21 @@ import { RenderBuilderContent } from "@/components/builder";
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
 export default async function BlogPostPage({ params }: any) {
-  const slug = params?.slug;
-  const urlPath = `/blog/${slug}`;
-
-  const content = await builder
-    .get("blog-post", {
-      userAttributes: {
-        urlPath,
-      },
-    })
-    .toPromise();
-
-  return content ? (
-    <RenderBuilderContent content={content} model="blog-post" />
-  ) : (
-    <div>⚠️ Blog post not found</div>
-  );
-}
+    // You need to await `params` first
+    const { slug } = await params; // Await the params
+  
+    const urlPath = `/blog/${slug}`;
+  
+    const content = await builder
+      .get("blog-post", {
+        userAttributes: { urlPath },
+      })
+      .toPromise();
+  
+    return content ? (
+      <RenderBuilderContent content={content} model="blog-post" />
+    ) : (
+      <div>⚠️ Blog post not found</div>
+    );
+  }
+  
